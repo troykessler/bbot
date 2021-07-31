6568a1cfe2d2e1bbd8df60fb3040898a4a747aa9
Troy Kessler <troy_kessler@protonmail.com>
2021-07-31 11:52:44 +0200

    added test scheduler

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-require('dotenv').config()
+require('dotenv').config();
+
+const scheduler = require('node-schedule');
 
 const Slimbot = require('slimbot');
 const slimbot = new Slimbot(process.env.BOT_TOKEN);
 
-setInterval(() => slimbot.sendMessage(343754242, 'Message received'), 3000)
+const birthdays = [
+  {
+    name: 'Test 1',
+    date: new Date(2021, 6, 31, 12, 0, 0)
+  },
+  {
+    name: 'Test 2',
+    date: new Date(2021, 6, 31, 12, 1, 0)
+  }
+]
+
+birthdays.forEach(birthday => {
+  scheduler.scheduleJob(birthday.date, () => {
+    slimbot.sendMessage(343754242, `Alles Gute zum Geburtstag ${birthday.name}!`);
+  })
+})
 
 slimbot.startPolling();
