cd7ff8266d16ec6d22e8c24bda2ab81469a094cb
Troy Kessler <troy_kessler@protonmail.com>
2021-07-31 12:04:42 +0200

    added test

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const slimbot = new Slimbot(process.env.BOT_TOKEN);
 const birthdays = [
   {
     name: 'Test 1',
-    date: new Date(2021, 6, 31, 12, 0, 0)
+    date: new Date(2021, 6, 31, 12, 5, 0)
   },
   {
     name: 'Test 2',
-    date: new Date(2021, 6, 31, 12, 1, 0)
+    date: new Date(2021, 6, 31, 12, 6, 0)
   }
 ]
 
 birthdays.forEach(birthday => {
   scheduler.scheduleJob(birthday.date, () => {
-    slimbot.sendMessage(343754242, `Alles Gute zum Geburtstag ${birthday.name}!`);
+    slimbot.sendMessage(343754242, `Alles Gute zum Geburtstag ${birthday.name}! 🎉🎉`);
   })
 })
 
