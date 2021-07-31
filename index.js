3ecaaa713f406b869e04ea79a0c0c249499f48a8
Troy Kessler <troy_kessler@protonmail.com>
2021-07-31 12:26:20 +0200

    changed timezone

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 require('dotenv').config();
 
-const scheduler = require('node-schedule');
+const Slimbot = require('slimbot');
 const express = require('express');
+
+const slimbot = new Slimbot(process.env.BOT_TOKEN);
+const scheduler = require('node-schedule');
+
 const app = express();
 
 app.get('/', (_, res) => {
   res.send(`StonesEmpireBot - ${new Date().toUTCString()}`);
 });
 
-const Slimbot = require('slimbot');
-const slimbot = new Slimbot(process.env.BOT_TOKEN);
-
 const birthdays = [
   {
     name: 'Test 1',
-    date: new Date(2021, 6, 31, 10, 24, 0)
+    date: new Date(2021, 6, 31, 10, 27, 0)
   },
   {
-    name: 'Test 2',
-    date: new Date(2021, 6, 31, 10, 25, 0)
+    name: '@troykessler',
+    date: new Date(2021, 6, 31, 10, 27, 0)
   }
 ]
 
