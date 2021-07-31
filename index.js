d44a2ca0d679bb1a209de09d85fb85659b9c8c85
Troy Kessler <troy_kessler@protonmail.com>
2021-07-31 12:13:43 +0200

    added server

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 
 const scheduler = require('node-schedule');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send("StonesEmpireBot");
+});
+
+app.listen(process.env.PORT);
 
 const Slimbot = require('slimbot');
 const slimbot = new Slimbot(process.env.BOT_TOKEN);
@@ -8,11 +16,11 @@ const slimbot = new Slimbot(process.env.BOT_TOKEN);
 const birthdays = [
   {
     name: 'Test 1',
-    date: new Date(2021, 6, 31, 12, 11, 0)
+    date: new Date(2021, 6, 31, 12, 15, 0)
   },
   {
     name: 'Test 2',
-    date: new Date(2021, 6, 31, 12, 12, 0)
+    date: new Date(2021, 6, 31, 12, 16, 0)
   }
 ]
 
